7741e71cc7561d791752eef429e27fd2f06b6dfe
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-12-07 20:04:26 -0330

    daylistitem - format spots remaining text

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,9 +6,18 @@ const classNames = require('classnames');
 
 
 export default function DayListItem(props) {
-
+  
   const {name, spots, selected, setDay} = props;
+  
+  const formatSpots = (spots) => {
+    let formattedString = "";
+    formattedString += spots !== 0 ? `${spots} ` : "no ";
+    formattedString += spots !== 1 ? "spots " : "spot ";
+    formattedString += "remaining";
+    return formattedString;
 
+  }
+  
   const isFull = spots === 0;
 
   const liClass = classNames(
@@ -22,7 +31,7 @@ export default function DayListItem(props) {
   return (
     <li className={liClass} onClick={() => {setDay(name)}}>
       <h2 className="text--regular">{name}</h2> 
-      <h3 className="text--light">{spots} spots remaining</h3>
+  <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
