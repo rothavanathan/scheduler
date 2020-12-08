23c35c037e62d35682a54ac93c36e6004d0a7875
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-12-08 15:00:55 -0330

    interviewList - implement component so stories function as expected

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -6,13 +6,21 @@ import InterviewerListItem from "./InterviewerListItem";
 export default function InterviewerList(props) {
   const {interviewers, interviewer, setInterviewer} = props;
   
-
+  const results = interviewers.map(interviewerData => {
+    return <InterviewerListItem 
+    id={interviewerData.id} 
+    name={interviewerData.name}
+    avatar={interviewerData.avatar}
+    setInterviewer={setInterviewer}
+    selected={interviewerData.id === interviewer}
+    />
+  })
 
   return (
  <section className="interviewers">
-  <h4 className="interviewers__header text--light">{interviewer}</h4>
+  <h4 className="interviewers__header text--light">Interviewer</h4>
   <ul className="interviewers__list">
-    interviewerListItems go here
+    {results}
   </ul>
 </section>
 
