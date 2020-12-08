1625d2f8707bf5ad68b66e040e622b3421e067b4
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-12-08 13:31:14 -0330

    interviewerListItem - stories execute as expected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "components/DayListItem.scss";
+import "components/InterviewerListItem.scss";
 
 const classNames = require('classnames');
 
@@ -8,15 +8,28 @@ const classNames = require('classnames');
 export default function InterviewerListItem(props) {
   const {id, name, avatar, selected, setInterviewer} = props
 
+  const interviewerClasses = classNames({
+    "interviewers__item": true,
+    
+    "interviewers__item--selected": selected
+  })
+  
+  const interviewerImgClasses = classNames({
+    "interviewers__item-image": true,
+    
+    "interviewers__item-image--selected": selected
+  })
 
   return (
-  <li className="interviewers__item">
+  <li 
+    className={interviewerClasses}
+    key={id}
+    onClick={()=> setInterviewer(name)}>
     <img
-      className="interviewers__item-image"
+      className={interviewerImgClasses}
       src={avatar}
       alt={name}
-    /> {name}
+    /> {selected ? name : ""}
   </li>
-)
-
-}
+  )
+};  
