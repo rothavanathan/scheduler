5709ad7915e2a0e23be7bf3de9e41965ce1af220
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-12-08 16:39:03 -0330

    Empty - add component and story to show button

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import "./styles.scss";
-
+import Header from "./Header.js"
+import Empty from "./Empty.js"
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment">I AM appointment</article>
+    <article className="appointment">
+    </article>
   )
 };  
--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import DayListItem from "components/DayListItem";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js"
+import Header from "components/Appointment/Header.js"
+import Empty from "components/Appointment/Empty.js"
 
 
 storiesOf("Button", module)
@@ -143,3 +145,5 @@ storiesOf("DayList", module)
       })
       .add("Appointment", () => <Appointment />)
       .add("Appointment with Time", () => <Appointment time="12pm"/>)
+      .add("Header",() => <Header time="12pm" />) 
+      .add("Empty", () => <Empty onAdd={action("onAdd")} />)
