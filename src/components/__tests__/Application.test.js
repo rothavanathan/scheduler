bef3a9fcadb089f116b0408c7bfbe3c8d286aa2d
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-12-15 20:37:50 -0330

    application - test delete interview userflows

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -63,5 +63,41 @@ describe("Application", ()=> {
 
     expect(getByText(monday, "no spots remaining")).toBeInTheDocument()
   })
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    //render
+    const {container} = render(<Application />);
+    //check if initial render shows all appointments
+    await waitForElement(() => getByText(container, `Archie Cohen`));
+
+    expect(getByText(container, `Archie Cohen`)).toBeInTheDocument();
+    //select appointment that has SHOW interview as node
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[1];
+    //click on the trash button and check if node transitioned to confirm delete
+    
+    fireEvent.click(getByAltText(appointment, "Delete"))
+    expect(getByText(appointment, `Delete the appointment?`)).toBeInTheDocument();
+    //click on the cancel button and check if node transitioned to SHOW appoinment
+    fireEvent.click(getByText(appointment, "Cancel"))
+    expect(getByText(appointment, `Archie Cohen`)).toBeInTheDocument();
+    //click on the trash button and check if node transitioned to CONFIRM appoinment delete
+    fireEvent.click(getByAltText(appointment, "Delete"))
+    expect(getByText(appointment, `Delete the appointment?`)).toBeInTheDocument();
+    
+    //click on confirm delete
+    fireEvent.click(getByText(appointment, "Confirm"))
+
+    //Wait until the delete is confirmed
+    await waitForElement(() => getByTitle(appointment, `add_button`));
+    
+    expect(getByText(appointment, `Archie Cohen`)).not.toBeInTheDocument();
+    console.log(prettyDOM(appointment))
+    
+    //check if appointment node is now EMPTY
+
+
+
+  })
 })
 
